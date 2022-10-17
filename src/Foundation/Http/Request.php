d8f5219210900501f4bcf18a2df52377b5d7f32d
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-17 23:35:39 +0200

    feat(http): implement base request

---
+++ src/Foundation/Http/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Foundation\Http;
+
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Base request class.
+ */
+class Request
+{
+    public function __construct(protected RequestStack $requestStack) { }
+
+    /**
+     * Get current request.
+     *
+     * @psalm-suppress NullableReturnStatement - we know that request is not null
+     * @psalm-suppress InvalidNullableReturnType - we know that request is not null
+     */
+    public function request(): SymfonyRequest
+    {
+        return $this->requestStack->getCurrentRequest();
+    }
+
+    /**
+     * Returns all request parameters.
+     */
+    public function all(): array
+    {
+        return $this->request()->request->all();
+    }
+}
