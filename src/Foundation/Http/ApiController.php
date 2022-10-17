af181e910249a37f6ad704bb76725dd9df5ab235
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-17 23:46:01 +0200

    chore(domain): code refactoring

---
+++ src/Foundation/Http/ApiController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Foundation\Http;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Base controller for all API controllers.
+ */
+abstract class ApiController extends AbstractController
+{
+
+}
