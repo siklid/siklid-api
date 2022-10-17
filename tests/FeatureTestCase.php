a2afce31976f9403afb3d05b0f39e128b1d5d79c
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-17 21:44:17 +0200

    chore(tests): create base class for functional tests

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+/**
+ * FeatureTestCase is the base class for functional tests.
+ */
+class FeatureTestCase extends WebTestCase
+{
+    /**
+     * A wrapper for the static createClient() method.
+     *
+     * @param array $options An array of options to pass to the createKernel method
+     * @param array $server  An array of server parameters
+     */
+    protected function createCrawler(array $options = [], array $server = []): KernelBrowser
+    {
+        return self::createClient($options, $server);
+    }
+
+    /**
+     * Asserts that the response status code is 200.
+     *
+     * @param string $message Assertion message
+     */
+    protected function assertResponseIsOk(string $message = ''): void
+    {
+        self::assertResponseStatusCodeSame(200, $message);
+    }
+
+    /**
+     * Asserts that the response status code is 201.
+     *
+     * @param string $message Assertion message
+     */
+    protected function assertResponseIsCreated(string $message = ''): void
+    {
+        self::assertResponseStatusCodeSame(201, $message);
+    }
+
+    /**
+     * Asserts that the response status code is 404.
+     *
+     * @param string $message Assertion message
+     */
+    protected function assertResponseIsNotFound(string $message = ''): void
+    {
+        self::assertResponseStatusCodeSame(404, $message);
+    }
+}
