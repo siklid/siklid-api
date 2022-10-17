703ece56354aefd5dcc7e716db0edb1be34d6f95
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-17 11:33:27 +0200

    feat(auth): add key pair generator

---
+++ bin/keypair.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Generate private.key and public.key if one of them is missing
+if [ ! -f private.key ] || [ ! -f public.key ]; then
+    # Get OAUTH_PASSPHRASE from .env
+    OAUTH_PASSPHRASE=$(grep OAUTH_PASSPHRASE .env | cut -d '=' -f2)
+    # Generate private.key
+    openssl genrsa -aes256 -passout pass:"$OAUTH_PASSPHRASE" -out private.key 4096
+    # Generate public.key
+    openssl rsa -passin pass:"$OAUTH_PASSPHRASE" -pubout -in private.key -out public.key
+else
+    echo "private.key and public.key already exist"
+fi
