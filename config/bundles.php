75c58d77121ea65fb7de86d3c1442e9d9dcd964c
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-17 21:03:12 +0200

    chore(console): install the maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
