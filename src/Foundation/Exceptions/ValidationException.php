aca71f5528f70a3ab404b59064fc44f0c517eec9
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-20 14:55:43 +0200

    feat(validation): return json response from errors

--- src/Foundation/Exceptions/ValidationException.php
+++ src/Foundation/Exceptions/ValidationException.php
@@ -6,8 +6,10 @@ namespace App\Foundation\Exceptions;
 
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormErrorIterator;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
-class ValidationException extends SiklidException
+class ValidationException extends SiklidException implements RenderableInterface
 {
     protected ?FormErrorIterator $errorIterator = null;
 
@@ -17,5 +19,43 @@ class ValidationException extends SiklidException
     public function setErrorIterator(FormErrorIterator $errorIterator): void
     {
         $this->errorIterator = $errorIterator;
+    }
+
+    /**
+     * Get the response that should be returned.
+     */
+    public function render(): Response
+    {
+        return new JsonResponse([
+            'message' => 'Invalid request',
+            'errors' => $this->formatErrorMessages(),
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    private function formatErrorMessages(): array
+    {
+        $errors = [];
+
+        if (null === $this->errorIterator) {
+            return $errors;
+        }
+
+        foreach ($this->errorIterator as $error) {
+            /** @psalm-suppress MixedAssignment, PossiblyUndefinedMethod, MixedMethodCall */
+            $propertyPath = $error->getCause()->getPropertyPath();
+
+            /** @psalm-suppress MixedAssignment, PossiblyUndefinedMethod */
+            $errorMessage = $error->getMessage();
+
+            /** @var string $propertyPath */
+            if (! isset($errors[$propertyPath])) {
+                $errors[$propertyPath] = [];
+            }
+
+            /** @psalm-suppress MixedAssignment */
+            $errors[$propertyPath][] = $errorMessage;
+        }
+
+        return $errors;
     }
 }
