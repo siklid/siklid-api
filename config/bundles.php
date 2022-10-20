84a82cf88df5a0d818c15ccd926851f906aac658
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-20 21:42:50 +0200

    chore(composer): install jwt bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,9 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
