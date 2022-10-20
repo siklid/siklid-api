cc8d8ed66aa461bd9deb4ab3a363c2563e3753d2
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-20 13:37:36 +0200

    chore(security): install security bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,9 +1,8 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
