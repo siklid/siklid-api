8519c808af3356a9d1c50f7e9ab40cb0b94b98d7
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-21 19:48:34 +0200

    fix styling

--- src/Siklid/Auth/Requests/RegisterByEmailRequest.php
+++ src/Siklid/Auth/Requests/RegisterByEmailRequest.php
@@ -10,6 +10,6 @@ final class RegisterByEmailRequest extends Request
 {
     public function formInput(): array
     {
-        return (array)($this->all()['user'] ?? []);
+        return (array) ($this->all()['user'] ?? []);
     }
 }
