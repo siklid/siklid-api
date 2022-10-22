164cb78d1f7cab4acb8841b4d57880711eabf874
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-22 11:09:24 +0200

    chore(exception): add documentation and todos

--- src/Foundation/Exception/SiklidException.php
+++ src/Foundation/Exception/SiklidException.php
@@ -6,6 +6,10 @@ namespace App\Foundation\Exception;
 
 use RuntimeException;
 
+/**
+ * Class SiklidException
+ * All application exceptions should extend this class.
+ */
 class SiklidException extends RuntimeException
 {
 }
