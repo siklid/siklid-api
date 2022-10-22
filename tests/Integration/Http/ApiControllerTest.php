ef66573218995b215d14fe768f5bb030ea650888
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-22 16:55:12 +0200

    chore(tests): test api controller

---
+++ tests/Integration/Http/ApiControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Integration\Http;
+
+use App\Foundation\Http\ApiController;
+use App\Tests\IntegrationTestCase;
+
+/**
+ * @psalm-suppress MissingConstructor
+ */
+class ApiControllerTest extends IntegrationTestCase
+{
+    /**
+     * @test
+     */
+    public function created(): void
+    {
+        $sut = new class extends ApiController {
+            public function __invoke(): void
+            {
+                $this->created('foo');
+            }
+        };
+        $sut->setContainer($this->container());
+
+        $response = $sut->created('foo', ['bar']);
+
+        $this->assertSame(201, $response->getStatusCode());
+        $this->assertSame('{"data":"foo"}', $response->getContent());
+    }
+}
