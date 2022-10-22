372f606bae000f8b421706babd0f82eb94be9f38
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-22 14:10:02 +0200

    chore(tests): add container and console helpers

--- tests/IntegrationTestCase.php
+++ tests/IntegrationTestCase.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Tests;
 
 use App\Foundation\Util\Json;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * IntegrationTestCase is the base class for integration tests.
@@ -28,5 +30,28 @@ class IntegrationTestCase extends KernelTestCase
 
         $this->faker = Faker::create();
         $this->json = new Json();
+    }
+
+    /**
+     * Provides a dedicated test container
+     * It's a wrapper around the static::getContainer() method
+     */
+    protected function container(): ContainerInterface
+    {
+        return static::getContainer();
+    }
+
+    /**
+     * Creates a dedicated console application
+     *
+     * @param array $options The kernel options
+     *
+     * @return Application
+     */
+    protected function consoleApplication(array $options = []): Application
+    {
+        $kernel = self::bootKernel($options);
+
+        return new Application($kernel);
     }
 }
