fd58685c5e179ef565bb8d640d2d6fdd788bd109
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-27 11:44:39 +0200

    feat(mongo): register username type

--- src/Foundation/ValueObject/UsernameType.php
+++ src/Foundation/ValueObject/UsernameType.php
@@ -4,6 +4,20 @@ declare(strict_types=1);
 
 namespace App\Foundation\ValueObject;
 
-class UsernameType
+use Doctrine\ODM\MongoDB\Types\ClosureToPHP;
+use Doctrine\ODM\MongoDB\Types\Type;
+
+class UsernameType extends Type
 {
+    use ClosureToPHP;
+
+    public function convertToDatabaseValue($value): string
+    {
+        return (string)$value;
+    }
+
+    public function convertToPHPValue($value): ?Username
+    {
+        return is_string($value) ? Username::fromString($value) : null;
+    }
 }
