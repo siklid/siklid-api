7263dacae6985be05ad1db579fe4521b3909f0fd
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-27 22:32:04 +0200

    chore(exception): cover exception handler with tests

--- src/Foundation/Exception/RenderableInterface.php
+++ src/Foundation/Exception/RenderableInterface.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\Foundation\Exception;
 
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
-interface RenderableInterface
+interface RenderableInterface extends Throwable
 {
     /**
      * Get the response that should be returned.
