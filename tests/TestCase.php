d37f243814e5be2b669ddd0facc1b56f390f299b
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-27 22:54:06 +0200

    chore(tests): add json util

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests;
 
+use App\Foundation\Util\Json;
+
 /**
  * Class TestCase
  * All Unit tests should extend this class.
@@ -14,10 +16,14 @@ class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected Faker $faker;
 
+    protected Json $json;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->faker = Faker::create();
+
+        $this->json = new Json();
     }
 }
