b70e225f471d6b649e0e1766e5b39ba7a8020a6e
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-28 11:04:10 +0200

    chore(vo): email is json serializable

--- tests/Unit/Foundation/ValueObject/EmailTest.php
+++ tests/Unit/Foundation/ValueObject/EmailTest.php
@@ -7,7 +7,11 @@ namespace App\Tests\Unit\Foundation\ValueObject;
 use App\Foundation\Exception\ValidationException;
 use App\Foundation\ValueObject\Email;
 use App\Tests\TestCase;
+use JsonException;
 
+/**
+ * @psalm-suppress MissingConstructor - We don't need a constructor.
+ */
 class EmailTest extends TestCase
 {
     /**
@@ -48,5 +52,19 @@ class EmailTest extends TestCase
         $other = Email::fromString($value);
 
         $this->assertTrue($sut->equals($other));
+    }
+
+    /**
+     * @test
+     * @psalm-suppress MixedAssignment - $value is a string
+     * @psalm-suppress MixedArgument - $value is a string
+     * @throws JsonException
+     */
+    public function json_serialize(): void
+    {
+        $value = $this->faker->email();
+        $sut = Email::fromString($value);
+
+        $this->assertSame('"'.$value.'"', json_encode($sut, JSON_THROW_ON_ERROR));
     }
 }
