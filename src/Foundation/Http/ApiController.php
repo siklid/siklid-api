27ae5fa92ac776199ee7fd6e64cc5812f64b178a
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-29 11:06:21 +0200

    feat(http): implement `ok` method

--- src/Foundation/Http/ApiController.php
+++ src/Foundation/Http/ApiController.php
@@ -6,18 +6,34 @@ namespace App\Foundation\Http;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Base controller for all API controllers.
  */
 abstract class ApiController extends AbstractController
 {
+    /**
+     * Returns a JSON response with a 200 status code.
+     */
+    public function ok(mixed $data, array $groups = [], array $headers = [], array $context = []): JsonResponse
+    {
+        $context = array_replace_recursive($context, [
+            'groups' => $groups,
+        ]);
+
+        return $this->json(compact('data'), Response::HTTP_OK, $headers, $context);
+    }
+
+    /**
+     * Returns a JSON response with a 201 status code.
+     */
     public function created(mixed $data, array $groups = [], array $headers = [], array $context = []): JsonResponse
     {
         $context = array_replace_recursive($context, [
             'groups' => $groups,
         ]);
 
-        return $this->json(compact('data'), 201, $headers, $context);
+        return $this->json(compact('data'), Response::HTTP_CREATED, $headers, $context);
     }
 }
--- tests/Unit/Foundation/Http/ApiControllerTest.php
+++ tests/Unit/Foundation/Http/ApiControllerTest.php
@@ -7,24 +7,48 @@ namespace App\Tests\Unit\Foundation\Http;
 use App\Foundation\Http\ApiController;
 use App\Tests\TestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @psalm-suppress MissingConstructor
  */
 class ApiControllerTest extends TestCase
 {
+    private ApiController $sut;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->sut = $this->getMockForAbstractClass(ApiController::class);
+        $this->sut->setContainer($this->createMock(ContainerInterface::class));
+    }
+
+    /**
+     * @test
+     */
+    public function ok(): void
+    {
+        $data = ['foo' => 'bar'];
+
+        $actual = $this->sut->ok($data);
+
+        $this->assertSame(Response::HTTP_OK, $actual->getStatusCode());
+        $content = $this->json->jsonToArray((string)$actual->getContent());
+        $this->assertSame($data, $content['data']);
+    }
+
+
     /**
      * @test
      */
     public function created(): void
     {
-        $sut = $this->getMockForAbstractClass(ApiController::class);
-        $sut->setContainer($this->createMock(ContainerInterface::class));
         $data = ['foo' => 'bar'];
 
-        $actual = $sut->created($data);
+        $actual = $this->sut->created($data);
 
-        $this->assertSame(201, $actual->getStatusCode());
+        $this->assertSame(Response::HTTP_CREATED, $actual->getStatusCode());
         $content = $this->json->jsonToArray((string)$actual->getContent());
         $this->assertSame($data, $content['data']);
     }
