d420eeeed735f006d732a454c0daa07334c82217
Mohamad Eldhemy <imdhemy@gmail.com>
2022-10-29 12:15:43 +0200

    feat(auth): implement login success handler

---
+++ src/Siklid/Application/Auth/LoginSuccessHandler.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Siklid\Application\Auth;
+
+use App\Foundation\Http\ApiController;
+use App\Foundation\Security\Token\TokenManagerInterface;
+use App\Siklid\Document\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+/**
+ * Authentication success handler.
+ */
+class LoginSuccessHandler extends ApiController implements AuthenticationSuccessHandlerInterface
+{
+    private TokenManagerInterface $tokenManager;
+
+    public function __construct(TokenManagerInterface $tokenManager)
+    {
+        $this->tokenManager = $tokenManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): Response
+    {
+        $data = $this->getResponseData($token);
+
+        $groups = ['user:read', 'token:read'];
+
+        return $this->ok($data, $groups);
+    }
+
+    /**
+     * @param TokenInterface $token
+     * @return array
+     */
+    public function getResponseData(TokenInterface $token): array
+    {
+        /** @var User $user */
+        $user = $token->getUser();
+        $accessToken = $this->tokenManager->createAccessToken($user);
+        $user->setAccessToken($accessToken);
+
+        return [
+            'user' => $user,
+            'token' => $user->getAccessToken(),
+        ];
+    }
+}
