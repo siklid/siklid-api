5e7e340f72c1669b928c4360a334716546e3efba
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-04 20:15:42 +0200

    chore(json): add unit tests

--- tests/Unit/Foundation/Util/JsonTest.php
+++ tests/Unit/Foundation/Util/JsonTest.php
@@ -14,6 +14,31 @@ class JsonTest extends TestCase
 {
     private Json $sut;
 
+    /**
+     * @return array[]
+     */
+    public function array_to_json_encodes_array_into_json_data_provider(): array
+    {
+        return [
+            'empty array' => [
+                'array' => [],
+                'expected' => '[]',
+            ],
+            'array containing empty array' => [
+                'array' => [[]],
+                'expected' => '[[]]',
+            ],
+            'array with one element' => [
+                'array' => ['foo' => 'bar'],
+                'expected' => '{"foo":"bar"}',
+            ],
+            'array with multiple elements' => [
+                'array' => ['foo' => 'bar', 'baz' => 'qux'],
+                'expected' => '{"foo":"bar","baz":"qux"}',
+            ],
+        ];
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -37,12 +62,37 @@ class JsonTest extends TestCase
     /**
      * @test
      */
-    public function json_to_array_returns_empty_if_json_is_invalid(): void
+    public function json_to_array_returns_empty_array_if_json_is_invalid(): void
     {
         $json = 'invalid json';
         $expected = [];
 
         $actual = $this->sut->jsonToArray($json);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     *
+     * @dataProvider array_to_json_encodes_array_into_json_data_provider
+     */
+    public function array_to_json_returns_a_valid_json(array $array, string $expected): void
+    {
+        $actual = $this->sut->arrayToJson($array);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    /**
+     * @test
+     */
+    public function array_to_json_returns_empty_array_json_on_failure(): void
+    {
+        $array = ['foo' => fopen('php://memory', 'rb')];
+        $expected = '[]';
+
+        $actual = $this->sut->arrayToJson($array);
 
         $this->assertSame($expected, $actual);
     }
