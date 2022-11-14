2008921e573b8bcea89d70d11399afe4936d28aa
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-14 16:04:55 +0200

    box description is optional

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -42,7 +42,7 @@ class FeatureTestCase extends WebTestCase
      * A wrapper for the static createClient() method.
      *
      * @param array $options An array of options to pass to the createKernel method
-     * @param array $server An array of server parameters
+     * @param array $server  An array of server parameters
      */
     protected function createCrawler(array $options = [], array $server = []): KernelBrowser
     {
@@ -80,7 +80,7 @@ class FeatureTestCase extends WebTestCase
     }
 
     /**
-     * Asserts that the response status code is 400
+     * Asserts that the response status code is 400.
      *
      * @param string $message Assertion message
      */
