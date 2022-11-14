36a2d8dc0a837cbf19dab114253371b3259522a3
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-14 17:40:20 +0200

    add repetition algorithm type

---
+++ src/Siklid/Application/Contract/Type/RepetitionAlgorithm.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Siklid\Application\Contract\Type;
+
+/**
+ * Repetition algorithm type.
+ */
+enum RepetitionAlgorithm: string
+{
+    case Leitner = 'leitner';
+}
