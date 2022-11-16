d014c9592ea4f934c2b18823d56aa7a99e725951
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-16 18:50:29 +0200

    handle access denied exception

--- tests/Unit/Foundation/Exception/ExceptionListenerTest.php
+++ tests/Unit/Foundation/Exception/ExceptionListenerTest.php
@@ -10,6 +10,8 @@ use App\Tests\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 /**
@@ -40,5 +42,46 @@ class ExceptionListenerTest extends TestCase
         $sut->onKernelException($exceptionEvent);
 
         $this->assertSame($response, $exceptionEvent->getResponse());
+    }
+
+    /**
+     * @test
+     */
+    public function on_kernel_exception_gets_response_from_bad_request_http_exception(): void
+    {
+        $exception = new BadRequestHttpException('Bad request');
+        $exceptionEvent = new ExceptionEvent(
+            $this->createMock(HttpKernelInterface::class),
+            $this->createMock(Request::class),
+            1,
+            $exception
+        );
+
+        $sut = new ExceptionListener();
+
+        $sut->onKernelException($exceptionEvent);
+
+        $this->assertSame(Response::HTTP_BAD_REQUEST, $exceptionEvent->getResponse()?->getStatusCode());
+        $this->assertSame('{"message":"Bad request"}', $exceptionEvent->getResponse()?->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function on_kernel_exception_gets_response_from_access_denied_exception(): void
+    {
+        $exception = new AccessDeniedHttpException('Access denied');
+        $exceptionEvent = new ExceptionEvent(
+            $this->createMock(HttpKernelInterface::class),
+            $this->createMock(Request::class),
+            1,
+            $exception
+        );
+        $sut = new ExceptionListener();
+
+        $sut->onKernelException($exceptionEvent);
+
+        $this->assertSame(Response::HTTP_FORBIDDEN, $exceptionEvent->getResponse()?->getStatusCode());
+        $this->assertSame('{"message":"Access denied"}', $exceptionEvent->getResponse()?->getContent());
     }
 }
