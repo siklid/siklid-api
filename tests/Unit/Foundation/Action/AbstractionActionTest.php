f5640ff6e2cd18d213117c9fbaa9f3db5b8f6b82
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-20 11:32:36 +0200

    chore(action): add helper method to get config

--- tests/Unit/Foundation/Action/AbstractionActionTest.php
+++ tests/Unit/Foundation/Action/AbstractionActionTest.php
@@ -8,6 +8,8 @@ use App\Foundation\Action\AbstractAction;
 use App\Foundation\Action\ValidatableInterface;
 use App\Foundation\Exception\ValidationException;
 use App\Tests\TestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\Form\FormInterface;
 
 /**
@@ -52,5 +54,46 @@ class AbstractionActionTest extends TestCase
         $sut = $this->getMockForAbstractClass(AbstractAction::class);
 
         $sut->validate($form, $this->request);
+    }
+
+    /**
+     * @test
+     */
+    public function get_config(): AbstractAction
+    {
+        $sut = $this->getMockForAbstractClass(AbstractAction::class);
+        $container = $this->createMock(ContainerInterface::class);
+        $container->method('has')->willReturn(true);
+        $parameterBag = $this->createMock(ParameterBag::class);
+        $parameterBag->method('get')->willReturn(['foo' => ['bar' => 'baz']]);
+        $container->method('get')->willReturn($parameterBag);
+
+        $sut->setContainer($container);
+
+        $this->assertSame('baz', $sut->getConfig('foo.bar'));
+        $this->assertSame(['bar' => 'baz'], $sut->getConfig('foo'));
+        $this->assertNull($sut->getConfig('missing'));
+
+        return $sut;
+    }
+
+    /**
+     * @test
+     *
+     * @depends get_config
+     */
+    public function get_config_returns_null_by_default(AbstractAction $sut): void
+    {
+        $this->assertNull($sut->getConfig('foo.missing'));
+    }
+
+    /**
+     * @test
+     *
+     * @depends get_config
+     */
+    public function get_config_can_accept_a_default_value(AbstractAction $sut): void
+    {
+        $this->assertSame('default', $sut->getConfig('foo.missing', 'default'));
     }
 }
