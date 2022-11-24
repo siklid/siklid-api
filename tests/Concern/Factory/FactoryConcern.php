6043fd2e5701816d83ca3b76236c13faa9599cde
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-24 11:48:03 +0200

    chore(tests): add docs

--- tests/Concern/Factory/FactoryConcern.php
+++ tests/Concern/Factory/FactoryConcern.php
@@ -7,6 +7,10 @@ namespace App\Tests\Concern\Factory;
 use App\Tests\Concern\WithFaker;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
+/**
+ * This trait should be used by all factory traits.
+ * It makes sure that the container is available and that the faker is loaded.
+ */
 trait FactoryConcern
 {
     use WithFaker;
