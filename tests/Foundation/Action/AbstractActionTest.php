a16f3decfae52da6dd837f4f0494387a78590333
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-27 19:51:43 +0200

    fix(tests): fix test name

--- tests/Foundation/Action/AbstractActionTest.php
+++ tests/Foundation/Action/AbstractActionTest.php
@@ -7,7 +7,7 @@ namespace App\Tests\Foundation\Action;
 use App\Foundation\Action\AbstractAction;
 use App\Foundation\Action\ValidatableInterface;
 use App\Foundation\Exception\ValidationException;
-use App\Tests\TestCase;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 use Symfony\Component\Form\FormInterface;
@@ -15,7 +15,7 @@ use Symfony\Component\Form\FormInterface;
 /**
  * @psalm-suppress MissingConstructor
  */
-class AbstractionActionTest extends TestCase
+class AbstractActionTest extends TestCase
 {
     private ValidatableInterface $request;
 
