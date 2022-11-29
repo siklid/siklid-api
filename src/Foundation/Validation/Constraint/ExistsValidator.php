a6d4b651bb5d5da3a1e3e225e0565738749f057e
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-29 16:51:02 +0200

    feat(validation): add exists validator

--- src/Foundation/Validation/Constraint/ExistsValidator.php
+++ src/Foundation/Validation/Constraint/ExistsValidator.php
@@ -4,6 +4,56 @@ declare(strict_types=1);
 
 namespace App\Foundation\Validation\Constraint;
 
-class ExistsValidator
+use Doctrine\ODM\MongoDB\DocumentManager;
+use InvalidArgumentException;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class ExistsValidator extends ConstraintValidator
 {
+    private DocumentManager $dm;
+
+    public function __construct(DocumentManager $dm)
+    {
+        $this->dm = $dm;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function validate(mixed $value, Constraint $constraint): void
+    {
+        if (empty($value)) {
+            return;
+        }
+
+        if (! $constraint instanceof Exists) {
+            throw new UnexpectedTypeException($constraint, Exists::class);
+        }
+
+        if (empty($constraint->document)) {
+            throw new InvalidArgumentException('Document class must be specified.');
+        }
+
+        if (empty($constraint->field)) {
+            throw new InvalidArgumentException('Field must be specified.');
+        }
+
+        if (! class_exists($constraint->document)) {
+            throw new InvalidArgumentException(sprintf('Document class "%s" does not exist.', $constraint->document));
+        }
+
+        $document = $this->dm->getRepository($constraint->document)->findOneBy([
+            $constraint->field => $value,
+        ]);
+
+        if (null === $document) {
+            $this->context->buildViolation($constraint->message())
+                ->setParameter('{{ document }}', $constraint->document)
+                ->setParameter('{{ field }}', $constraint->field)
+                ->setParameter('{{ value }}', (string)$value)
+                ->addViolation();
+        }
+    }
 }
---
+++ tests/Foundation/Validation/Constraint/ExistsValidatorTest.php
@@ -0,0 +1,136 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Foundation\Validation\Constraint;
+
+use App\Foundation\Validation\Constraint\Exists;
+use App\Foundation\Validation\Constraint\ExistsValidator;
+use App\Siklid\Document\User;
+use App\Tests\Concern\Factory\UserFactoryTrait;
+use App\Tests\Concern\KernelTestCaseTrait;
+use App\Tests\TestCase;
+use InvalidArgumentException;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Context\ExecutionContext;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class ExistsValidatorTest extends TestCase
+{
+    use KernelTestCaseTrait;
+    use UserFactoryTrait;
+
+    /**
+     * @test
+     */
+    public function it_validates_exists_constraint(): void
+    {
+        $constraint = new class() extends Constraint {
+        };
+        $sut = new ExistsValidator($this->getDocumentManager());
+
+        $this->expectException(UnexpectedTypeException::class);
+
+        $sut->validate('foo', $constraint);
+    }
+
+    /**
+     * @test
+     */
+    public function it_skips_empty_values(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        $sut = new ExistsValidator($this->getDocumentManager());
+
+        $sut->validate(null, new Exists());
+        $sut->validate('', new Exists());
+    }
+
+    /**
+     * @test
+     */
+    public function it_requires_document(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage('Document class must be specified.');
+
+        $sut = new ExistsValidator($this->getDocumentManager());
+
+        $sut->validate('foo', new Exists());
+    }
+
+    /**
+     * @test
+     */
+    public function it_requires_field(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage('Field must be specified.');
+
+        $sut = new ExistsValidator($this->getDocumentManager());
+
+        $sut->validate('foo', new Exists(['document' => 'foo', 'field' => '']));
+    }
+
+    /**
+     * @test
+     */
+    public function document_class_should_exist(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage('Document class "foo" does not exist.');
+
+        $sut = new ExistsValidator($this->getDocumentManager());
+
+        $sut->validate('foo', new Exists(['document' => 'foo', 'field' => 'bar']));
+    }
+
+    /**
+     * @test
+     *
+     * @psalm-suppress InternalClass
+     * @psalm-suppress InternalMethod
+     */
+    public function it_adds_violation_if_not_exists(): void
+    {
+        $sut = new ExistsValidator($this->getDocumentManager());
+        $context = new ExecutionContext(
+            $this->createMock(ValidatorInterface::class),
+            'root',
+            $this->createMock(TranslatorInterface::class)
+        );
+        $sut->initialize($context);
+
+        $sut->validate('bar', new Exists(['document' => User::class, 'field' => 'foo']));
+
+        $this->assertCount(1, $context->getViolations());
+    }
+
+    /**
+     * @test
+     *
+     * @psalm-suppress InternalClass
+     * @psalm-suppress InternalMethod
+     */
+    public function it_passes_if_data_exists(): void
+    {
+        $user = $this->makeUser();
+        $this->persistDocument($user);
+        $sut = new ExistsValidator($this->getDocumentManager());
+        $context = new ExecutionContext(
+            $this->createMock(ValidatorInterface::class),
+            'root',
+            $this->createMock(TranslatorInterface::class)
+        );
+        $sut->initialize($context);
+
+        $sut->validate($user->getId(), new Exists(['document' => User::class]));
+        $sut->validate($user->getId(), new Exists(['document' => User::class, 'field' => 'id']));
+        $sut->validate($user->getUsername(), new Exists(['document' => User::class, 'field' => 'username']));
+
+        $this->assertCount(0, $context->getViolations());
+    }
+}
