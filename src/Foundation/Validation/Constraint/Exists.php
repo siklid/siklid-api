154bca56d14783f74e2a297930ea62dd722aa17d
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-29 17:23:39 +0200

    feat(validation): add named arguments

--- src/Foundation/Validation/Constraint/Exists.php
+++ src/Foundation/Validation/Constraint/Exists.php
@@ -5,14 +5,34 @@ declare(strict_types=1);
 namespace App\Foundation\Validation\Constraint;
 
 use Attribute;
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 use Symfony\Component\Validator\Constraint;
 
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
 #[Attribute]
 class Exists extends Constraint
 {
-    public string $field = 'id';
+    public string $field;
 
-    public ?string $document = null;
+    public ?string $document;
+
+    /**
+     * @psalm-suppress MixedArgumentTypeCoercion
+     */
+    #[HasNamedArguments]
+    public function __construct(
+        string $field = 'id',
+        ?string $document = null,
+        array $groups = null,
+        mixed $payload = null
+    ) {
+        parent::__construct([], $groups, $payload);
+
+        $this->field = $field;
+        $this->document = $document;
+    }
 
     public function validatedBy(): string
     {
--- tests/Foundation/Validation/Constraint/ExistsTest.php
+++ tests/Foundation/Validation/Constraint/ExistsTest.php
@@ -8,6 +8,7 @@ use App\Foundation\Validation\Constraint\Exists;
 use App\Foundation\Validation\Constraint\ExistsValidator;
 use App\Tests\TestCase;
 use Attribute;
+use Symfony\Component\Validator\Attribute\HasNamedArguments;
 
 class ExistsTest extends TestCase
 {
@@ -19,6 +20,16 @@ class ExistsTest extends TestCase
         $sut = new Exists();
 
         $this->assertHasAttribute($sut, Attribute::class);
+    }
+
+    /**
+     * @test
+     */
+    public function has_named_arguments(): void
+    {
+        $sut = new Exists();
+
+        $this->assertMethodHasAttribute($sut, '__construct', HasNamedArguments::class);
     }
 
     /**
