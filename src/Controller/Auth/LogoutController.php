c87979aebdb1ec7598a651a878f123b9757690ad
Moemen Gaballa <moemengaballa@gmail.com>
2022-12-08 17:05:14 +0200

    logout done success

--- src/Controller/Auth/LogoutController.php
+++ src/Controller/Auth/LogoutController.php
@@ -4,35 +4,42 @@ declare(strict_types=1);
 
 namespace App\Controller\Auth;
 
-use App\Controller\TokenAuthenticatedInterface;
 use App\Foundation\Http\ApiController;
+use App\Foundation\Redis\Contract\SetInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
 
-class LogoutController extends ApiController implements TokenAuthenticatedInterface
+class LogoutController extends ApiController
 {
     protected array $invalidTokens = array();
     #[Route('/auth/logout', name: 'auth_logout', methods: ['POST'])]
-    public function logout(Request $request, CacheInterface $cache): Response
+    public function logout(Request $request, CacheInterface $cache, SetInterface $set): Response
     {
         $tokenWithBearer = $request->headers->get('Authorization');
 
-        $tokenWithoutBearer = str_replace('Bearer ', '', $tokenWithBearer);
+//        $tokenWithoutBearer = str_replace('Bearer ', '', $tokenWithBearer);
 
-        $cacheItem = $cache->getItem('users.invalidToken');
+        $setKey = 'invalidTokens';
+        $set->add($setKey, $tokenWithBearer);
 
-        if (! $cacheItem->isHit()) {
-            $this->invalidTokens[$tokenWithoutBearer] = time();
-        } else {
-            $this->invalidTokens = $cacheItem->get();
-            $this->invalidTokens[$tokenWithoutBearer] = time();
-        }
-
-        $cacheItem->set($this->invalidTokens);
-
-        $cache->save($cacheItem);
+//        $values = $set->members($setKey);
+//
+//
+//        $values = $set->members($setKey1);
+//        $cacheItem = $cache->getItem('users.invalidToken');
+//
+//        if (! $cacheItem->isHit()) {
+//            $this->invalidTokens[$tokenWithoutBearer] = time();
+//        } else {
+//            $this->invalidTokens = $cacheItem->get();
+//            $this->invalidTokens[$tokenWithoutBearer] = time();
+//        }
+//
+//        $cacheItem->set($this->invalidTokens);
+//
+//        $cache->save($cacheItem);
 
         $data = [
             'message' => 'Done logout successfully',
--- src/EventSubscriber/TokenSubscriber.php
+++ src/EventSubscriber/TokenSubscriber.php
@@ -3,6 +3,7 @@
 namespace App\EventSubscriber;
 
 use App\Controller\TokenAuthenticatedInterface;
+use App\Foundation\Redis\Contract\SetInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,31 +13,32 @@ use Symfony\Contracts\Cache\CacheInterface;
 
 class TokenSubscriber implements EventSubscriberInterface
 {
-    private $tokens;
-    private $cache;
+//    private $tokens;
+//    private $cache;
+    private $set;
 
-    public function __construct(CacheInterface $cache)
+    public function __construct(SetInterface $set)
     {
-        $this->cache = $cache;
+        $this->set = $set;
     }
 
     public function onKernelController(ControllerEvent $event)
     {
-        $this->tokens =  $this->cache->getItem('users.invalidToken')->get();
-        $controller = $event->getController();
+//        $this->tokens =  $this->cache->getItem('users.invalidToken')->get();
+//        $controller = $event->getController();
 
         // when a controller class defines multiple action methods, the controller
         // is returned as [$controllerInstance, 'methodName']
-        if (is_array($controller)) {
-            $controller = $controller[0];
-        }
+//        if (is_array($controller)) {
+//            $controller = $controller[0];
+//        }
 
-        if ($controller instanceof TokenAuthenticatedInterface) {
+        if ($event->getRequest()->headers->has('Authorization')) {
 
+            $values = $this->set->members('invalidTokens');
             $tokenWithBearer = $event->getRequest()->headers->get('Authorization');
-            $tokenWithoutBearer = str_replace('Bearer ', '', $tokenWithBearer);
-
-            if (isset($this->tokens[$tokenWithoutBearer])) {
+//            dd($values, $tokenWithBearer, isset($values[$tokenWithBearer]));
+            if(in_array($tokenWithBearer, $values)){
                 $data = [
                     "code"=> Response::HTTP_UNAUTHORIZED,
                     'message' => 'Invalid JWT Token',
@@ -45,9 +47,22 @@ class TokenSubscriber implements EventSubscriberInterface
                 $event->setController(function() use ($data){
                     return new JsonResponse(json_encode($data), Response::HTTP_UNAUTHORIZED, [], true);
                 });
-
-
             }
+
+//            $tokenWithoutBearer = str_replace('Bearer ', '', $tokenWithBearer);
+//
+//            if (isset($this->tokens[$tokenWithoutBearer])) {
+//                $data = [
+//                    "code"=> Response::HTTP_UNAUTHORIZED,
+//                    'message' => 'Invalid JWT Token',
+//                ];
+//
+//                $event->setController(function() use ($data){
+//                    return new JsonResponse(json_encode($data), Response::HTTP_UNAUTHORIZED, [], true);
+//                });
+//
+//
+//            }
         }
     }
 
