aeb1490bd7d383ed87d01981a1675f8fd2e6a2e9
Moemen Gaballa <moemengaballa@gmail.com>
2022-12-09 08:46:33 +0200

    remove some comments and refactor pslam

--- src/Controller/Auth/LogoutController.php
+++ src/Controller/Auth/LogoutController.php
@@ -9,37 +9,17 @@ use App\Foundation\Redis\Contract\SetInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Cache\CacheInterface;
 
 class LogoutController extends ApiController
 {
     protected array $invalidTokens = array();
     #[Route('/auth/logout', name: 'auth_logout', methods: ['POST'])]
-    public function logout(Request $request, CacheInterface $cache, SetInterface $set): Response
+    public function logout(Request $request, SetInterface $set): Response
     {
         $tokenWithBearer = $request->headers->get('Authorization');
 
-//        $tokenWithoutBearer = str_replace('Bearer ', '', $tokenWithBearer);
-
         $setKey = 'invalidTokens';
         $set->add($setKey, $tokenWithBearer);
-
-//        $values = $set->members($setKey);
-//
-//
-//        $values = $set->members($setKey1);
-//        $cacheItem = $cache->getItem('users.invalidToken');
-//
-//        if (! $cacheItem->isHit()) {
-//            $this->invalidTokens[$tokenWithoutBearer] = time();
-//        } else {
-//            $this->invalidTokens = $cacheItem->get();
-//            $this->invalidTokens[$tokenWithoutBearer] = time();
-//        }
-//
-//        $cacheItem->set($this->invalidTokens);
-//
-//        $cache->save($cacheItem);
 
         $data = [
             'message' => 'Done logout successfully',
--- src/EventSubscriber/TokenSubscriber.php
+++ src/EventSubscriber/TokenSubscriber.php
@@ -2,19 +2,15 @@
 
 namespace App\EventSubscriber;
 
-use App\Controller\TokenAuthenticatedInterface;
 use App\Foundation\Redis\Contract\SetInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Contracts\Cache\CacheInterface;
 
 class TokenSubscriber implements EventSubscriberInterface
 {
-//    private $tokens;
-//    private $cache;
     private $set;
 
     public function __construct(SetInterface $set)
@@ -22,22 +18,11 @@ class TokenSubscriber implements EventSubscriberInterface
         $this->set = $set;
     }
 
-    public function onKernelController(ControllerEvent $event)
+    public function onKernelController(ControllerEvent $event): bool|Response
     {
-//        $this->tokens =  $this->cache->getItem('users.invalidToken')->get();
-//        $controller = $event->getController();
-
-        // when a controller class defines multiple action methods, the controller
-        // is returned as [$controllerInstance, 'methodName']
-//        if (is_array($controller)) {
-//            $controller = $controller[0];
-//        }
-
         if ($event->getRequest()->headers->has('Authorization')) {
-
             $values = $this->set->members('invalidTokens');
             $tokenWithBearer = $event->getRequest()->headers->get('Authorization');
-//            dd($values, $tokenWithBearer, isset($values[$tokenWithBearer]));
             if(in_array($tokenWithBearer, $values)){
                 $data = [
                     "code"=> Response::HTTP_UNAUTHORIZED,
@@ -49,21 +34,8 @@ class TokenSubscriber implements EventSubscriberInterface
                 });
             }
 
-//            $tokenWithoutBearer = str_replace('Bearer ', '', $tokenWithBearer);
-//
-//            if (isset($this->tokens[$tokenWithoutBearer])) {
-//                $data = [
-//                    "code"=> Response::HTTP_UNAUTHORIZED,
-//                    'message' => 'Invalid JWT Token',
-//                ];
-//
-//                $event->setController(function() use ($data){
-//                    return new JsonResponse(json_encode($data), Response::HTTP_UNAUTHORIZED, [], true);
-//                });
-//
-//
-//            }
         }
+        return true;
     }
 
     public static function getSubscribedEvents()
