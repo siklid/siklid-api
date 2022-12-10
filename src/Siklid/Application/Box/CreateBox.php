29dbb7cbfdb29764489c0c0ca845d84d3af5416b
Mohamad Eldhemy <imdhemy@gmail.com>
2022-12-10 16:11:15 +0200

    chore(box): use hashtag utility

--- src/Siklid/Application/Box/CreateBox.php
+++ src/Siklid/Application/Box/CreateBox.php
@@ -6,6 +6,7 @@ namespace App\Siklid\Application\Box;
 
 use App\Foundation\Action\AbstractAction;
 use App\Foundation\Http\Request;
+use App\Foundation\Util\Hashtag;
 use App\Foundation\Validation\ValidatorInterface;
 use App\Siklid\Application\Contract\Entity\BoxInterface;
 use App\Siklid\Document\Box;
@@ -18,12 +19,14 @@ final class CreateBox extends AbstractAction
     private DocumentManager $dm;
 
     private ValidatorInterface $validator;
+    private Hashtag $hashtag;
 
-    public function __construct(Request $request, DocumentManager $dm, ValidatorInterface $validator)
+    public function __construct(Request $request, DocumentManager $dm, ValidatorInterface $validator, Hashtag $hashtag)
     {
         $this->request = $request;
         $this->dm = $dm;
         $this->validator = $validator;
+        $this->hashtag = $hashtag;
     }
 
     public function execute(): BoxInterface
@@ -34,7 +37,8 @@ final class CreateBox extends AbstractAction
         assert(is_string($description) || is_null($description));
         $box->setDescription($description);
         $box->setUser($this->getUser());
-        $box->setHashtags($this->extractHashtags($box->getDescription()));
+        $hashtags = $this->hashtag->extract((string)$description);
+        $box->setHashtags($hashtags);
 
         $this->validator->validate($box);
 
@@ -42,20 +46,5 @@ final class CreateBox extends AbstractAction
         $this->dm->flush();
 
         return $box;
-    }
-
-    private function extractHashtags(?string $text): array
-    {
-        if (null === $text) {
-            return [];
-        }
-
-        $hashtags = [];
-        preg_match_all('/#(\S+)/', $text, $matches);
-        foreach ($matches[0] as $match) {
-            $hashtags[] = mb_strtolower($match);
-        }
-
-        return $hashtags;
     }
 }
