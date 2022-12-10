ac23a293dc5d335e6a12fc729a18f62dfe0929f0
Mohamad Eldhemy <imdhemy@gmail.com>
2022-12-10 16:24:29 +0200

    feat(box): add create box request

--- src/Siklid/Application/Box/CreateBox.php
+++ src/Siklid/Application/Box/CreateBox.php
@@ -5,24 +5,28 @@ declare(strict_types=1);
 namespace App\Siklid\Application\Box;
 
 use App\Foundation\Action\AbstractAction;
-use App\Foundation\Http\Request;
 use App\Foundation\Util\Hashtag;
 use App\Foundation\Validation\ValidatorInterface;
+use App\Siklid\Application\Box\Request\CreateBoxRequest;
 use App\Siklid\Application\Contract\Entity\BoxInterface;
 use App\Siklid\Document\Box;
 use Doctrine\ODM\MongoDB\DocumentManager;
 
 final class CreateBox extends AbstractAction
 {
-    private Request $request;
+    private CreateBoxRequest $request;
 
     private DocumentManager $dm;
 
     private ValidatorInterface $validator;
     private Hashtag $hashtag;
 
-    public function __construct(Request $request, DocumentManager $dm, ValidatorInterface $validator, Hashtag $hashtag)
-    {
+    public function __construct(
+        CreateBoxRequest $request,
+        DocumentManager $dm,
+        ValidatorInterface $validator,
+        Hashtag $hashtag
+    ) {
         $this->request = $request;
         $this->dm = $dm;
         $this->validator = $validator;
@@ -31,14 +35,9 @@ final class CreateBox extends AbstractAction
 
     public function execute(): BoxInterface
     {
-        $box = new Box();
-        $box->setName((string)$this->request->get('name'));
-        $description = $this->request->get('description');
-        assert(is_string($description) || is_null($description));
-        $box->setDescription($description);
+        $box = $this->fill(Box::class, $this->request->formInput());
         $box->setUser($this->getUser());
-        $hashtags = $this->hashtag->extract((string)$description);
-        $box->setHashtags($hashtags);
+        $box->setHashtags($this->hashtag->extract((string)$box->getDescription()));
 
         $this->validator->validate($box);
 
