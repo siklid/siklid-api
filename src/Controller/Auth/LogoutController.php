12caedd1bb1cf398198b3c279217c2f462423469
moemen gaballa <moemengaballa@gmail.com>
2022-12-12 14:34:16 +0200

    delete refresh token when logout

--- src/Controller/Auth/LogoutController.php
+++ src/Controller/Auth/LogoutController.php
@@ -7,17 +7,44 @@ namespace App\Controller\Auth;
 use App\Foundation\Http\ApiController;
 use App\Foundation\Redis\Contract\SetInterface;
 use App\Siklid\Application\Auth\Request\LogoutRequest;
+use App\Siklid\Document\RefreshToken;
+use \Gesdinet\JWTRefreshTokenBundle\Document\RefreshTokenRepository;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
 
 class LogoutController extends ApiController
 {
+    private DocumentManager $documentManager;
+//    private RefreshTokenRepository $refreshTokenRepository;
+
+    public function __construct(
+        DocumentManager $documentManager,
+//        RefreshTokenRepository $refreshTokenRepository
+    ) {
+        $this->documentManager = $documentManager;
+//        $this->refreshTokenRepository = $refreshTokenRepository;
+    }
+
     #[Route('/auth/logout', name: 'auth_logout', methods: ['POST'])]
     public function logout(LogoutRequest $request, SetInterface $set): Response
     {
+        $refreshTokenRepository = $this->documentManager->getRepository(RefreshToken::class);
+        assert($refreshTokenRepository instanceof RefreshTokenRepository);
+        $refreshToken = $refreshTokenRepository->findOneBy(['refreshToken' => $request->all()['refreshToken']]);
 
-//        dd($request->all()['refreshToken']);
+        if (!$refreshToken instanceof RefreshToken) {
+            throw new NotFoundHttpException(
+                sprintf('RefreshToken with value [%s] cannot be found.', $refreshToken)
+            );
+        }
+
+        $this->documentManager->remove($refreshToken);
+        $this->documentManager->flush();
+
         $tokenWithBearer = $request->request()->headers->get('Authorization');
         $userId = $this->getUser()->getId();
 
