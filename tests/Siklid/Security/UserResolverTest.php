522b6a27c9201ee7fd168fb2576c4857c1d19cc3
Mohamad Eldhemy <imdhemy@gmail.com>
2022-12-17 16:45:17 +0200

    feat(security): implement user resolver

---
+++ tests/Siklid/Security/UserResolverTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Siklid\Security;
+
+use App\Siklid\Document\User;
+use App\Siklid\Security\UserResolver;
+use App\Tests\Concern\KernelTestCaseTrait;
+use App\Tests\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class UserResolverTest extends TestCase
+{
+    use KernelTestCaseTrait;
+
+    /**
+     * @test
+     */
+    public function get_user_returns_null_with_no_active_user(): void
+    {
+        /** @var TokenStorageInterface $tokenStorage */
+        $tokenStorage = $this->container()->get('security.token_storage');
+        $sut = new UserResolver($tokenStorage);
+
+        $actual = $sut->getUser();
+
+        self::assertNull($actual);
+    }
+
+    /**
+     * @test
+     */
+    public function get_user_returns_the_current_user(): void
+    {
+        $user = new User();
+        $token = $this->createMock(TokenInterface::class);
+        $token->method('getUser')->willReturn($user);
+        $tokenStorage = $this->createMock(TokenStorageInterface::class);
+        $tokenStorage->method('getToken')->willReturn($token);
+        $sut = new UserResolver($tokenStorage);
+
+        $actual = $sut->getUser();
+
+        $this->assertSame($user, $actual);
+    }
+}
