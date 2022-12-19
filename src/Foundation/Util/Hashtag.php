3d7df7ce527955981742b891485482936f77dc51
Mohamad Eldhemy <imdhemy@gmail.com>
2022-12-19 07:47:02 +0200

    chore(util): remove comment

--- src/Foundation/Util/Hashtag.php
+++ src/Foundation/Util/Hashtag.php
@@ -4,13 +4,6 @@ declare(strict_types=1);
 
 namespace App\Foundation\Util;
 
-/**
- * This class is used to extract hashtags from a text.
- * Current implementation is temporary and should be replaced with a more
- * robust solution in the context of this PR.
- *
- * @see {@link https://github.com/piscibus/siklid-api/pull/115}
- */
 final class Hashtag
 {
     public function extract(string $text): array
