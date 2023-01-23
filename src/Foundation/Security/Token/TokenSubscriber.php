a9c17e8a882d6ef8f2ea438fd1fb4493a4ba2f2c
Mohamad Eldhemy <imdhemy@gmail.com>
2023-01-23 18:45:18 +0200

    fix(security): handle post auth token

--- src/Foundation/Security/Token/TokenSubscriber.php
+++ src/Foundation/Security/Token/TokenSubscriber.php
@@ -6,6 +6,7 @@ namespace App\Foundation\Security\Token;
 
 use App\Foundation\Security\Token\TokenManagerInterface as Manager;
 use Lexik\Bundle\JWTAuthenticationBundle\Exception\ExpiredTokenException;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Authenticator\Token\JWTPostAuthenticationToken;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface as Storage;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -24,14 +25,14 @@ class TokenSubscriber implements EventSubscriberInterface
     public function verifyToken(): void
     {
         $token = $this->storage->getToken();
-        if (! $token) {
+        if (! $token instanceof JWTPostAuthenticationToken) {
             return;
         }
 
         $user = $token->getUser();
         assert($user instanceof UserInterface);
 
-        if ($this->manager->isAccessTokenRevokedForUser((string)$token, $user)) {
+        if ($this->manager->isAccessTokenRevokedForUser($token->getCredentials(), $user)) {
             throw new ExpiredTokenException();
         }
     }
