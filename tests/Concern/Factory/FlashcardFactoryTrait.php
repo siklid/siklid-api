778d43dd577487947709be8fb0aa2497fe55da35
Dhemy <imdhemy@gmail.com>
2023-02-18 18:53:02 +0100

    chore(tests): create flashcard factory
    Signed-off-by: Dhemy <imdhemy@gmail.com>

---
+++ tests/Concern/Factory/FlashcardFactoryTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Concern\Factory;
+
+use App\Siklid\Document\Flashcard;
+use Doctrine\Common\Collections\ArrayCollection;
+
+trait FlashcardFactoryTrait
+{
+    use FactoryConcern;
+
+    protected function makeFlashcard(array $attributes = []): Flashcard
+    {
+        $flashcard = new Flashcard();
+
+        $flashcard->setUser($attributes['user']);
+        $flashcard->setFront($attributes['front'] ?? $this->faker->sentence());
+        $flashcard->setBack($attributes['back'] ?? $this->faker->sentence());
+        $flashcard->setBoxes($attributes['boxes'] ?? new ArrayCollection());
+
+        $this->touchCollection(Flashcard::class);
+
+        return $flashcard;
+    }
+}
