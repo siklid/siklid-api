14a95bbc58d8b01e0ac61b33f3025a528e26b15a
Dhemy <imdhemy@gmail.com>
2023-02-19 12:17:27 +0100

    chore(flashcard): define flashcard  endpoint

--- src/Controller/FlashcardController.php
+++ src/Controller/FlashcardController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Foundation\Http\ApiController;
 use App\Siklid\Application\Flashcard\CreateFlashcard;
+use App\Siklid\Application\Flashcard\DeleteFlashcard;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +16,11 @@ class FlashcardController extends ApiController
     public function store(CreateFlashcard $action): JsonResponse
     {
         return $this->created($action->execute(), ['flashcard:create', 'resource:read']);
+    }
+
+    #[Route('/flashcards/{id}', name: 'flashcard_delete', methods: ['DELETE'])]
+    public function delete(DeleteFlashcard $action): JsonResponse
+    {
+        return $this->ok($action->execute(), ['flashcard:index', 'resource:read']);
     }
 }
