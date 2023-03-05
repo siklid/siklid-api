3f4bc47dadd534ab1002007cc89090ae263e4446
Dhemy <imdhemy@gmail.com>
2023-03-05 15:31:17 +0100

    feat(security): implement authorization checker

--- src/Foundation/Security/Authorization/AuthorizationCheckerInterface.php
+++ src/Foundation/Security/Authorization/AuthorizationCheckerInterface.php
@@ -12,18 +12,18 @@ interface AuthorizationCheckerInterface
     /**
      * Checks if the attribute is granted against the current authentication token and optionally supplied subject.
      *
-     * @param string[]|string            $attribute A single or an array of attributes to vote on
-     * @param AuthorizableInterface|null $subject   the subject to secure, The object the user wants to access
+     * @param string[]|string       $attribute A single or an array of attributes to vote on
+     * @param AuthorizableInterface $subject   the subject to secure, The object the user wants to access
      */
-    public function isGranted(array|string $attribute, ?AuthorizableInterface $subject = null): bool;
+    public function isGranted(array|string $attribute, AuthorizableInterface $subject): bool;
 
     /**
      * Throws an exception if the attribute is not granted against the current authentication token and optionally supplied subject.
      *
-     * @param string[]|string            $attribute A single or an array of attributes to vote on
-     * @param AuthorizableInterface|null $subject   the subject to secure, The object the user wants to access
+     * @param string[]|string       $attribute A single or an array of attributes to vote on
+     * @param AuthorizableInterface $subject   the subject to secure, The object the user wants to access
      *
      * @throws AccessDeniedException if access is not granted
      */
-    public function denyAccessUnlessGranted(array|string $attribute, ?AuthorizableInterface $subject = null): void;
+    public function denyAccessUnlessGranted(array|string $attribute, AuthorizableInterface $subject): void;
 }
