9180534828801c4b9974b6d025ab59d203aa0bc5
Mohamed Gamal <mg.dev1992@gmail.com>
2023-03-29 12:41:14 +0200

    fix typo

--- src/Siklid/Application/Flashcard/ViewFlashcard.php
+++ src/Siklid/Application/Flashcard/ViewFlashcard.php
@@ -6,18 +6,18 @@ namespace App\Siklid\Application\Flashcard;
 
 use App\Foundation\Action\AbstractAction;
 use App\Foundation\Exception\SiklidException;
-use App\Siklid\Application\Contract\Entity\FlashCardInterface;
+use App\Siklid\Application\Contract\Entity\FlashcardInterface;
 
 class ViewFlashcard extends AbstractAction
 {
-    private ?FlashCardInterface $flashCard;
+    private ?FlashcardInterface $flashCard;
 
-    public function __construct(?FlashCardInterface $flashCard = null)
+    public function __construct(?FlashcardInterface $flashCard = null)
     {
         $this->flashCard = $flashCard;
     }
 
-    public function execute(): FlashCardInterface
+    public function execute(): FlashcardInterface
     {
         if (null === $this->flashCard) {
             throw new SiklidException('Flashcard is not set');
@@ -26,7 +26,7 @@ class ViewFlashcard extends AbstractAction
         return $this->flashCard;
     }
 
-    public function setFlashcard(FlashCardInterface $flashCard): self
+    public function setFlashcard(FlashcardInterface $flashCard): self
     {
         $this->flashCard = $flashCard;
 
